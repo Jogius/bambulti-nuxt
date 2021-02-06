2bdf7ff9ec383252bf08653c8431cc361a7a67a9
Jogius <jogius.julius.makowski@gmail.com>
2021-02-06 15:47:26 +0100

    feat: animations

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,11 @@
               contain
               max-height="300"
               max-width="300"
-              class="Logo mx-auto"
+              class="Logo mx-auto slide-up"
               alt="Bambulti Logo"
               src="img/logo.png"
             />
-            <h1>Bambulti</h1>
+            <h1 class="slide-up">Bambulti</h1>
           </v-flex>
         </v-col>
       </v-row>
@@ -49,9 +49,9 @@
       <v-row justify="center" align="center">
         <v-col cols="6">
           <v-flex class="text-center">
-            <h1>Die Bambultea-Flask</h1>
+            <h1 class="slide-left">Die Bambultea-Flask</h1>
             <v-divider />
-            <p>
+            <p class="slide-right">
               Unser erstes Produkt war die Bambultea-Flask, eine Thermosflasche
               aus Edelstahl und echtem Bambus mit 500ml Fassungsverm&ouml;gen,
               Teesieb und zwei s&uuml;&szlig;en Babypandas als Aufdruck.
@@ -63,7 +63,7 @@
             contain
             :aspect-ratio="135 / 169"
             max-width="500"
-            class="Bottle mx-auto"
+            class="Bottle mx-auto slide-left"
             alt="Bambultea-Flask"
             src="img/bambultea-flask-detail.jpg"
           />
@@ -77,16 +77,16 @@
             contain
             :aspect-ratio="1 / 1"
             max-width="500"
-            class="Bottle mx-auto"
+            class="Bottle mx-auto slide-right"
             alt="Bambulti Team"
             src="img/team.jpg"
           />
         </v-col>
         <v-col cols="12" sm="6">
           <v-flex class="text-center">
-            <h1>&Uuml;ber uns</h1>
+            <h1 class="slide-right">&Uuml;ber uns</h1>
             <v-divider />
-            <p>
+            <p class="slide-left">
               Wir sind das Bambulti-Team, eine neunk&ouml;pfige Gruppe aus
               Sch&uuml;lern des F.-F.-Runge Gymnasiums in Oranienburg.
             </p>
@@ -98,7 +98,31 @@
 </template>
 
 <script>
-export default {}
+import Scrollreveal from 'scrollreveal'
+export default {
+  mounted() {
+    Scrollreveal().reveal('.slide-up', {
+      origin: 'bottom',
+      delay: 200,
+      distance: '150px',
+      scale: 0.5,
+    })
+    Scrollreveal().reveal('.slide-left', {
+      origin: 'right',
+      delay: 400,
+      distance: '150px',
+      opacity: 0,
+      scale: 0.5,
+    })
+    Scrollreveal().reveal('.slide-right', {
+      origin: 'left',
+      delay: 400,
+      distance: '150px',
+      opacity: 0,
+      scale: 0.5,
+    })
+  },
+}
 </script>
 
 <style scoped>
