ad31eb0a5da4cce00f3129da04dfecb91b70fd69
Jogius <jogius.julius.makowski@gmail.com>
2021-04-16 15:35:18 +0200

    fix: error background

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,6 @@
 <template>
   <v-flex class="text-center">
+    <div id="parallax" />
     <v-icon x-large>mdi-emoticon-sad-outline</v-icon>
     <h1 v-text="error.statusCode === 404 ? pageNotFound : otherError" />
     <v-btn x-large icon to="/"><v-icon x-large>mdi-home</v-icon></v-btn>
@@ -30,3 +31,18 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+#parallax {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  max-width: 100%;
+  opacity: 0.35;
+  background-repeat: repeat;
+  background-image: url('~assets/img/background_pattern.png');
+  overflow-y: hidden;
+}
+</style>
